39508799f3916f9d4cc09ba00bbaa66c6dfdad45
Erico Gomes <jose.erico@ulife.com.br>
2024-11-11 20:50:47 -0300

    Projeto base dev.

--- src/minimaxjogodavelha/logica/MiniMax.java
+++ src/minimaxjogodavelha/logica/MiniMax.java
@@ -13,6 +13,7 @@ import java.util.Random;
  */
 public class MiniMax {
 
+
     private int[][] ganharCombinacoes = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9},
     {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}};
 
