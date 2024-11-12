91ee1129198ea663da0b12ab30d0ac42ae37550b
Erico Gomes <jose.erico@ulife.com.br>
2024-11-11 21:01:34 -0300

    Alteração de busca.

--- src/minimaxjogodavelha/logica/MiniMax.java
+++ src/minimaxjogodavelha/logica/MiniMax.java
@@ -17,6 +17,7 @@ public class MiniMax {
     private int[][] ganharCombinacoes = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9},
     {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}};
 
+
     // Metodo de AI
     public int min_max(int[] board) {
         int melhorVal = -1000000;
